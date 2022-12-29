0b476252a074ee0a742a99746bcd0d1566311017
Dalevy5689 <damian.levy81@gmail.com>
2022-12-29 03:46:16 -0500

    "Updated React App"

---
+++ xayamaca-coffee-website/src/components/Header.js
@@ -0,0 +1,53 @@
+import XaymacaCoffeeLogo from "../app/assets/img/XaymacaCoffee Main Logo.png";
+
+const Header = () => {
+  return (
+    <header className="header" id="header">
+      <nav className="nav container">
+        <a href="#home" className="nav__logo">
+          <img
+            src={XaymacaCoffeeLogo}
+            width={120}
+            alt="Xaymaca Coffee Logo"
+            className="float-start"
+          />
+        </a>
+
+        <div className="nav__menu" id="nav-menu">
+          <ul className="nav__list">
+            <li className="nav__item">
+              <a href="#home" className="nav__link active-link">
+                Home
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#products" className="nav__link">
+                Products
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#place" className="nav__link">
+                About Us
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#blog" className="nav__link">
+                Contact Us
+              </a>
+            </li>
+          </ul>
+
+          <div className="nav__close" id="nav-close">
+            <i className="bx bx-x"></i>
+          </div>
+        </div>
+
+        <div className="nav__toggle" id="nav-toggle">
+          <i className="bx bx-grid-alt"></i>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
