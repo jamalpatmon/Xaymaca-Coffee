dca17758ae9404d242eb2cae94cb2f97df857672
jamalpatmon <69773849+jamalpatmon@users.noreply.github.com>
2022-12-28 22:47:31 -0500

    Add files via upload

---
+++ aboutus.js
@@ -0,0 +1,31 @@
+const AboutPage = () => {
+  return (
+    <Container>
+      <SubHeader current={"About Us"} />
+      <Row className="row-content">
+        <Col sm="6">
+          <h3>Who Are We?</h3>
+          <p>
+            Xaymaca Coffee Traders was created in 2009 by the patriarch of the
+            Levy family, David Levy. He built our company on 2 simple tenets
+            excellent customer service and honesty. We, at XCT, will always call
+            you back, and we will always tell our customers and partners the
+            truth so they can run their businesses effectively. By staying true
+            to our values, Xaymaca has become the leading coffee importer and
+            distributor of Jamaica’s Blue Mountain Coffee and the US
+            representative of Coffee Traders Limited the #1 Jamaican Blue
+            Mountain coffee producer both in quantity and quality.
+          </p>
+
+          <h2>What Do We Do?</h2>
+          <p>
+            Xaymaca Coffee Traders sells both green coffee and roasted coffee
+            beans. We offer green coffee directly from the Jamaica Blue
+            Mountains and roasted specialty coffees from Jamaica and other
+            countries within the global coffee-producing belt.
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
