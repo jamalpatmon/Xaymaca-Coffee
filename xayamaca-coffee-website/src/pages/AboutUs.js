a20f6906d6dcc3481ca9d8a5b56b3b914075dce1
jamalpatmon <69773849+jamalpatmon@users.noreply.github.com>
2022-12-31 09:23:07 -0500

    Update AboutUs.js

--- xayamaca-coffee-website/src/pages/AboutUs.js
+++ xayamaca-coffee-website/src/pages/AboutUs.js
@@ -1,12 +1,20 @@
 import { Col, Row, Container } from "reactstrap";
+import coffeecup from "../app/assets/img/coffee.png";
+import SubHeader from "./SubHeader";
 
 const AboutUs = () => {
   return (
     <Container>
-      <Row className="row-content">
+      <SubHeader current={"About Us"} />
+      <Row className="row-content sm=3">
         <Col sm="6">
-          <h3>Who Are We?</h3>
-          <p>
+          <h2 id="h2" align={"left"}>
+            Who Are We?
+          </h2>
+
+          <br />
+
+          <p sm="3" align={"left"}>
             Xaymaca Coffee Traders was created in 2009 by the patriarch of the
             Levy family, David Levy. He built our company on 2 simple tenets
             excellent customer service and honesty. We, at XCT, will always call
@@ -17,8 +25,12 @@ const AboutUs = () => {
             representative of Coffee Traders Limited the #1 Jamaican Blue
             Mountain coffee producer both in quantity and quality.
           </p>
+        </Col>
+        <br />
 
-          <h2>What Do We Do?</h2>
+        <h2>What Do We Do?</h2>
+
+        <Col sm="6">
           <p>
             Xaymaca Coffee Traders sells both green coffee and roasted coffee
             beans. We offer green coffee directly from the Jamaica Blue
@@ -26,6 +38,14 @@ const AboutUs = () => {
             countries within the global coffee-producing belt.
           </p>
         </Col>
+
+        <img
+          src={coffeecup}
+          width={350}
+          alt="Coffee Cup"
+          className="float-right"
+          align={"right"}
+        />
       </Row>
     </Container>
   );
